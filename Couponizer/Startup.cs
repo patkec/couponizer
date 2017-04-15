bf695f71796bb8266d3f8f594671d14d1c2f82a0
Simon Stamcar <patkec@users.noreply.github.com>
2017-04-15 19:43:53 +0200

    Generate wwwroot on build

--- Couponizer/Startup.cs
+++ Couponizer/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -37,8 +38,24 @@ namespace Couponizer
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseStaticFiles();
+            // Redirect any non-API calls to the Angular application.
+            app.Use(async (context, next) => {
+                await next();
+                if (context.Response.StatusCode == 404 &&
+                    !Path.HasExtension(context.Request.Path.Value) &&
+                    !context.Request.Path.Value.StartsWith("/api/"))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
+
+            // COnfigure application to use default route for WebAPI
             app.UseMvcWithDefaultRoute();
+
+            // Configure application to serve index.html from /wwwroot
+            app.UseDefaultFiles(new DefaultFilesOptions {DefaultFileNames = new[] {"index.html"}});
+            app.UseStaticFiles();
         }
     }
 }
